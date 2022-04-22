8f5ce5fe93b6d0c095daa663e7b927cec5e416be
rajdeephu <rajdeephu@gmail.com>
2022-04-22 21:03:24 +0800

    include grayscale images

--- inpainter/__main__.py
+++ inpainter/__main__.py
@@ -7,9 +7,9 @@ import numpy as np
 def main():
     args = parse_args()
 
-    # image shape: (r, c, 3)
+    # image shape: (r, c, *) or (b, r, c, *) with * as 3 or 1
     # image values: 0 - 255
-    # mask shape: (r, c)
+    # mask shape: (r, c) or (b, r, c)
     # mask values: 1.0(parts to be filled in) or 0.0(parts not to be filled in)
     if '.npy' in args.input_image:
         image = np.load(args.input_image)
@@ -18,14 +18,25 @@ def main():
         image = imread(args.input_image)
         mask = imread(args.mask, as_gray=True)
 
-    output_image = Inpainter(
-        image,
-        mask,
-        patch_size=args.patch_size,
-        plot_progress=args.plot_progress
-    ).inpaint()
-    imsave(args.output, output_image, quality=100)
+    if args.grayscale:
+        image = np.repeat(image, 3, -1)
 
+    if len(image.shape) == 3:
+        image = np.expand_dims(image, axis=0)
+        mask = np.expand_dims(mask, axis=0)
+
+    for i in range(image.shape[0]):
+        output_image = Inpainter(
+            image[i],
+            mask[i],
+            patch_size=args.patch_size,
+            plot_progress=args.plot_progress
+        ).inpaint()
+
+        if args.grayscale:
+            output_image = output_image[:,:,0]
+        
+        imsave(args.output + str(i) + '.jpg', output_image, quality=100)
 
 def parse_args():
     parser = argparse.ArgumentParser()
@@ -40,13 +51,20 @@ def parse_args():
         '-o',
         '--output',
         help='the file path to save the output image',
-        default='output.jpg'
+        default='output/image'
     )
     parser.add_argument(
+        '-pp',
         '--plot-progress',
         help='plot each generated image',
         action='store_true',
         default=False
+    )
+    parser.add_argument(
+        '-gs',
+        '--grayscale',
+        help='input image is grayscale',
+        action='store_true'
     )
     parser.add_argument(
         'input_image',
